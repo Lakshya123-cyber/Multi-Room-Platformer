972c38d5a8ff863236cad46c4f64371881b5bb45
Lakshya123-cyber <lakshya.raikwal@giis.edu.sg>
2022-10-23 14:25:21 +0800

    Changes in player

--- index.js
+++ index.js
@@ -4,15 +4,42 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; // 1024
 canvas.height = 64 * 9; // 576
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100,
+    };
+    this.width = 100;
+    this.height = 100;
+    this.sides = {
+      bottom: this.position.y + this.height,
+    };
+  }
 
-c.fillStyle = "red";
-c.fillRect(100, 100, 100, 100);
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+
+  update() {
+    if (this.sides.bottom < canvas.height) {
+      this.position.y++;
+      this.sides.bottom = this.position.y + this.height;
+    }
+  }
+}
+
+const player = new Player();
+
+// let bottom = y + 100;
 
 function animate() {
   window.requestAnimationFrame(animate);
-  console.log("Animate");
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+  player.draw();
+  player.update();
 }
 
 animate();
