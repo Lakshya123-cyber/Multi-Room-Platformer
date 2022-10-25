04b5035043d339d416b56268c6a8aeba5a382b01
Lakshya123-cyber <lakshya.raikwal@giis.edu.sg>
2022-10-25 20:00:45 +0800

    Player movements pt2

--- index.js
+++ index.js
@@ -6,26 +6,30 @@ canvas.height = 64 * 9; // 576
 
 const player = new Player();
 
-// let bottom = y + 100;
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.velocity.x = 0;
+
+  if (keys.d.pressed) player.velocity.x = 5;
+  else if (keys.a.pressed) player.velocity.x = -5;
+
   player.draw();
   player.update();
 }
 
 animate();
-
-window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "w":
-      if (player.velocity.y === 0) player.velocity.y = -20;
-      break;
-    case "a":
-      break;
-    case "d":
-      break;
-  }
-});
