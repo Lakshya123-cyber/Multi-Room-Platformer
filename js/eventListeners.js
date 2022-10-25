46e2679cc2d57eccffef3f8762ee68fb337b3abf
Lakshya123-cyber <lakshya.raikwal@giis.edu.sg>
2022-10-25 20:06:16 +0800

    All Code

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -1,25 +1,50 @@
+window.addEventListener('keydown', (event) => {
+  if (player.preventInput) return
+  switch (event.key) {
+    case 'w':
+      for (let i = 0; i < doors.length; i++) {
+        const door = doors[i]
 
-window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "w":
-      if (player.velocity.y === 0) player.velocity.y = -20;
-      break;
-    case "a":
-      keys.a.pressed = true;
-      break;
-    case "d":
-      keys.d.pressed = true;
-      break;
-  }
-});
+        if (
+          player.hitbox.position.x + player.hitbox.width <=
+            door.position.x + door.width &&
+          player.hitbox.position.x >= door.position.x &&
+          player.hitbox.position.y + player.hitbox.height >= door.position.y &&
+          player.hitbox.position.y <= door.position.y + door.height
+        ) {
+          player.velocity.x = 0
+          player.velocity.y = 0
+          player.preventInput = true
+          player.switchSprite('enterDoor')
+          door.play()
+          return
+        }
+      }
+      if (player.velocity.y === 0) player.velocity.y = -25
 
-window.addEventListener("keyup", (e) => {
-  switch (e.key) {
-    case "a":
-      keys.a.pressed = false;
-      break;
-    case "d":
-      keys.d.pressed = false;
-      break;
+      break
+    case 'a':
+      // move player to the left
+      keys.a.pressed = true
+      break
+    case 'd':
+      // move player to the right
+      keys.d.pressed = true
+      break
   }
-});
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.key) {
+    case 'a':
+      // move player to the left
+      keys.a.pressed = false
+
+      break
+    case 'd':
+      // move player to the right
+      keys.d.pressed = false
+
+      break
+  }
+})
